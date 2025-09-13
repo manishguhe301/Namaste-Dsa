afc84fd284833518b9113bc1f5d0e65e520fc8dd
Manish <manishguhe301@gmail.com>
2025-09-13 14:10:42 +0530

    feat: pattern programming part1

---
+++ patterns/index.js
@@ -0,0 +1,29 @@
+let n = 4;
+
+for (let i = 0; i < n; i++) {
+  let row = ''
+  for (let j = 0; j < n; j++) {
+    row += "*  "
+  }
+  console.log(row);
+}
+
+console.log("");
+
+for (let i = 0; i < n; i++) {
+  let row = ''
+  for (let j = 0; j <= i; j++) {
+    row += "*  "
+  }
+  console.log(row);
+}
+
+console.log("");
+
+for (let i = 0; i <= n; i++) {
+  let row = ''
+  for (let j = 0; j <= i; j++) {
+    row += `${j + 1}  `
+  }
+  console.log(row);
+}
