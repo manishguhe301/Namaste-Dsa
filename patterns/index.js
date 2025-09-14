cda5825dcb7b418046a77f1d45b9ca4f674318cf
Manish <manishguhe301@gmail.com>
2025-09-14 15:34:37 +0530

    feat: patterns-intermediate

--- patterns/index.js
+++ patterns/index.js
@@ -1,4 +1,4 @@
-let n = 4;
+let n = 5;
 
 for (let i = 0; i < n; i++) {
   let row = ''
@@ -25,5 +25,78 @@ for (let i = 0; i <= n; i++) {
   for (let j = 0; j <= i; j++) {
     row += `${j + 1}  `
   }
+  console.log(row);
+}
+
+console.log('sds');
+
+for (let i = 0; i < 5; i++) {
+  let row = ''
+  for (let j = 0; j <= i; j++) {
+    row += `${i + 1}  `
+  }
+  console.log(row);
+}
+
+console.log('');
+
+for (let i = n; i >= 0; i--) {
+  let row = '';
+  for (let j = 0; j <= i; j++) {
+    row += `${j + 1}  `
+  }
+  console.log(row);
+}
+
+console.log("");
+
+for (let i = n; i >= 0; i--) {
+  let row = '';
+  for (let j = 0; j <= i; j++) {
+    row += '*  '
+  }
+  console.log(row);
+}
+
+console.log('');
+
+for (let i = 0; i < n; i++) {
+  let row = ''
+
+  for (let j = 0; j < n - (i + 1); j++) {
+    row += '-  '
+  }
+
+  for (let j = 0; j < i + 1; j++) {
+    row += "*  "
+  }
+
+  console.log(row);
+}
+
+
+console.log();
+
+for (let i = 0; i < 5; i++) {
+  let row = '';
+  let switchInt = 1;
+  for (let j = 0; j <= i; j++) {
+    row += `${switchInt}  `
+    switchInt = switchInt === 1 ? 0 : 1
+  }
+  console.log(row);
+}
+
+console.log();
+
+let toggle = 1
+for (let i = 0; i < n; i++) {
+  let row = '';
+
+  for (let j = 0; j <= i; j++) {
+    row += `${toggle} `
+    toggle = toggle === 1 ? 0 : 1
+  }
+
   console.log(row);
 }
