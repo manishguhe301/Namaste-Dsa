72f4ec87d6acce3adb2fa978b5a1f101ba7e1229
Manish <manishguhe301@gmail.com>
2025-10-02 14:13:16 +0530

    rec

---
+++ recursion/rec1.js
@@ -0,0 +1,20 @@
+let n = 10;
+
+function printN(n) {
+  if (n === 0) return;
+  console.log(n);
+  n--;
+  printN(n)
+}
+
+printN(n)
+
+let n1 = 10
+function print1ToN(x) {
+  if (x > n1) return;
+  console.log(x);
+  x++;
+  print1ToN(x)
+}
+
+print1ToN(1)
