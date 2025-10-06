10dff4b807fb08460fde70c6d6eab9d646e28b67
Manish <manishguhe301@gmail.com>
2025-10-06 12:09:24 +0530

    commented the code out

--- recursion/rec1.js
+++ recursion/rec1.js
@@ -1,36 +1,38 @@
-let n = 10;
+// let n = 10;
 
-function printN(n) {
-  if (n === 0) return;
-  console.log(n);
-  n--;
-  printN(n)
-}
+// function printN(n) {
+//   if (n === 0) return;
+//   console.log(n);
+//   n--;
+//   printN(n)
+// }
 
-printN(n)
+// printN(n)
 
-let n1 = 10
-function print1ToN(x) {
-  if (x > n1) return;
-  console.log(x);
-  x++;
-  print1ToN(x)
-}
+// let n1 = 10
+// function print1ToN(x) {
+//   if (x > n1) return;
+//   console.log(x);
+//   x++;
+//   print1ToN(x)
+// }
 
-print1ToN(1)
+// print1ToN(1)
 
-function sumOfNnumbers(n) {
-  if (n === 0) return 0;
-  return n + sumOfNnumbers(n - 1)
-}
+// function sumOfNnumbers(n) {
+//   if (n === 0) return 0;
+//   return n + sumOfNnumbers(n - 1)
+// }
 
-console.log(sumOfNnumbers(5));
+// console.log(sumOfNnumbers(5));
 
 
-let a = [1, 2, 3, 4, 5]
-function sumOfElements(n) {
-  if (n === 0) return a[0]
-  return a[n] + sumOfElements(n - 1)
-}
+// let a = [1, 2, 3, 4, 5]
+// function sumOfElements(n) {
+//   if (n === 0) return a[0]
+//   return a[n] + sumOfElements(n - 1)
+// }
 
-console.log(sumOfElements(a.length - 1));
+// console.log(sumOfElements(a.length - 1));
+
+// factorial of a number
